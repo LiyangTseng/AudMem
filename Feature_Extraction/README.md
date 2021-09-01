243c6931f6e6c9c0cfe0a5cbd7241804c3c5cabc
LiyangTseng <k00441tsengliyung@gmail.com>
2021-09-01 16:44:57 +0800

    done predicting emotions via PMEmo

--- Feature_Extraction/README.md
+++ Feature_Extraction/README.md
@@ -1,4 +1,37 @@
 # Feature Extraction
+## File Tree
+```
+.
+├── extract_features.py
+├── extract_labels.py
+├── features
+│   ├── chords
+│   │   ├── chroma
+│   │   └── tonnetz
+│   ├── emotions
+│   │   ├── dynamic_arousal
+│   │   ├── dynamic_valence
+│   │   ├── static_arousal
+│   │   ├── static_valence
+│   └── timbre
+│       ├── mfcc
+│       ├── mfcc_delta
+│       ├── mfcc_delta2
+│       ├── spectral_bandwidth
+│       ├── spectral_centroid
+│       ├── spectral_contrast
+│       ├── spectral_flatness
+│       └── spectral_rolloff
+├── labels
+│   └── track_memorability.csv
+│    ...  
+├── opensmile
+├── PMEmo
+├── README.md
+└── track_index.txt
+
+```
+
 ## File Usage
 - [experimentData_beta.csv](experimentData_beta.csv): experiment data exported from MySQL database. The data contains 4 fields as listed below: In particular, "audioOrder" is formatted as a string of 94-number-order separated by comma, whereas "userReponse" is formatted as a string of 162-number-reponse separated by comma.
 ```
@@ -8,10 +41,11 @@
 ```
     | tracktrack | repeat_interval:memorized |
 ```
-- [extract_features.py](extract_features.py): extract features from audio files. Features including chomagram(shape: 12, 646), tonnetz(shape: 6, 646), fluctation pattern, mfcc(12, 646), spectrum centroid(1, 646), spectrum bandwidth(1, 646), spectrum contrast(7, 646), spectrum flatness(1, 646), spectrum rolloff(1, 646), emotion.
+- [extract_features.py](extract_features.py): extract features from audio files. Features(shape) including chomagram(12, 646), tonnetz(6, 646), fluctation pattern, mfcc(12, 646), spectrum centroid(1, 646), spectrum bandwidth(1, 646), spectrum contrast(7, 646), spectrum flatness(1, 646), spectrum rolloff(1, 646), static arousal(1,) & valence(1,) predicted from PMEmo SVR linear kernel, and dynamic arousal(29,) & valence(29,) that are predicted from PMEmo SVR RBF kernel.
 - [track_index.txt](track_index.txt):  a look-up table that stores the relation between audio filename and index which is used in "AudioOrder" in [experimentData_beta.csv](experimentData_beta.csv).
 
-## Note
+## Other Notes
+### Opensmile lld configuration
 To extract the features for music emotion predictions, It is required to install [opensmile](https://github.com/audeering/opensmile)([alternative](https://github.com/naxingyu/opensmile)). You might also need to reference [this issue](https://github.com/audeering/opensmile/issues/14) to make extracting PMEmo dynamic features possible. One solution is to change 
 ```
 config_file = os.path.join(opensmiledir,"config", "IS13_ComParE_lld.conf")
@@ -25,3 +59,6 @@ config_file = os.path.join(opensmiledir,"config", "IS13_ComParE.conf")
 ...
 subprocess.check_call([SMILExtract, "-C", config_file, "-I", wavpath, "-lldcsvoutput", distfile])
 ```
+
+### PMEmo Model
+The model are trained in [features.ipynb]('features.ipynb) and stored as pkl file.
--- Feature_Extraction/extract_features.py
+++ Feature_Extraction/extract_features.py
@@ -1,11 +1,13 @@
 import os
 import numpy as np
+import pandas as pd
 import librosa
+import argparse
 from tqdm import tqdm
 from PMEmo.features import extract_all_wav_feature, extract_frame_feature, process_dynamic_feature
 import arff
 import pickle
-import sklearn
+from sklearn.preprocessing import StandardScaler
 
 AUDIO_DIR = '../Experiment_Website/clips'
 
@@ -14,25 +16,30 @@ def extract_chord_features():
 
     ''' extract choragram and tonnetz from LibROSA'''
 
-    chorma_dir = 'features/chords/chorma'
-    if not os.path.exists(chorma_dir):
-        os.makedirs(chorma_dir)
+    chroma_dir = 'features/chords/chroma'
+    if not os.path.exists(chroma_dir):
+        os.makedirs(chroma_dir)
     tonnetz_dir = 'features/chords/tonnetz'
     if not os.path.exists(tonnetz_dir):
         os.makedirs(tonnetz_dir)
 
     for audio_file in tqdm(os.listdir(AUDIO_DIR), desc='Chord Features', leave=True):
         audio_path = os.path.join(AUDIO_DIR, audio_file)
-        chorma_file = 'chroma_'+ audio_file[:16]
-        tonnetz_file = 'tonnetz_'+ audio_file[:16]
-        y, sr = librosa.load(audio_path)
-        chroma_cqt = librosa.feature.chroma_cens(y=y, sr=sr)
-        np.save(os.path.join(chorma_dir, chorma_file), chroma_cqt)
-        tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
-        np.save(os.path.join(tonnetz_dir, tonnetz_file), tonnetz)
+        chroma_path = os.path.join(chroma_dir, 'chroma_'+ audio_file[:16] + '.npy') 
+        tonnetz_path =  os.path.join(tonnetz_dir, 'tonnetz_'+ audio_file[:16] + '.npy')
+        if os.path.exists(chroma_path) and os.path.exists(tonnetz_path):
+            continue
+        else:
+            y, sr = librosa.load(audio_path)
+            if not os.path.exists(chroma_path):
+                chroma_cqt = librosa.feature.chroma_cens(y=y, sr=sr)
+                np.save(chroma_path, chroma_cqt)
+            if not os.path.exists(tonnetz_path):
+                tonnetz = librosa.feature.tonnetz(y=y, sr=sr)
+                np.save(tonnetz_path, tonnetz)
 
-    print('chroma extracted from {} and stored at {}'.format(AUDIO_DIR, chorma_dir))
-    print('tonnetz extracted from {} and stored at {}'.format(AUDIO_DIR, tonnetz_dir))
+    print('chroma features of {} saved at {}'.format(AUDIO_DIR, chroma_dir))
+    print('tonnetz features of {} saved at {}'.format(AUDIO_DIR, tonnetz_dir))
 
 def extract_rhythm_features():
     ''' extract rhythm pattern (FA) from MA Toolbox '''
@@ -72,44 +79,58 @@ def extract_timbre_features():
      
     for audio_file in tqdm(os.listdir(AUDIO_DIR), desc='Timbre Features', leave=True):
         audio_path = os.path.join(AUDIO_DIR, audio_file)
-        mfcc_file = 'mfcc_' + audio_file[:16]
-        mfcc_delta_file = 'mfccDelta_' + audio_file[:16]
-        mfcc_delta2_file = 'mfccDelta2_' + audio_file[:16]
-        spectral_centroid_file = 'cent_' + audio_file[:16]
-        spectral_bandwidth_file = 'spec_bw_' + audio_file[:16]
-        spectral_contrast_file = 'contrast_' + audio_file[:16]
-        spectral_flatness_file = 'flatness_' + audio_file[:16]
-        spectral_rolloff_file = 'rolloff_' + audio_file[:16]
-        y, sr = librosa.load(audio_path)
-        mfcc = librosa.feature.chroma_cens(y=y, sr=sr)
-        np.save(os.path.join(mfcc_dir, mfcc_file), mfcc)
-        mfcc_delta = librosa.feature.delta(mfcc)
-        np.save(os.path.join(mfcc_delta_dir, mfcc_delta_file), mfcc_delta)
-        mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
-        np.save(os.path.join(mfcc_delta2_dir, mfcc_delta2_file), mfcc_delta2)
-        cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-        np.save(os.path.join(spectral_centroid_dir, spectral_centroid_file), cent)
-        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
-        np.save(os.path.join(spectral_bandwidth_dir, spectral_bandwidth_file), spec_bw)
-        S = np.abs(librosa.stft(y))
-        contrast = librosa.feature.spectral_contrast(S=S, sr=sr)
-        np.save(os.path.join(spectral_centroid_dir, spectral_contrast_file), contrast)
-        flatness = librosa.feature.spectral_flatness(y=y)
-        np.save(os.path.join(spectral_flatness_dir, spectral_flatness_file), flatness)
-        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
-        np.save(os.path.join(spectral_rolloff_dir, spectral_rolloff_file), rolloff)
+        mfcc_path = os.path.join(mfcc_dir, 'mfcc_' + audio_file[:16] + '.npy')
+        mfcc_delta_path = os.path.join(mfcc_delta_dir, 'mfccDelta_' + audio_file[:16] + '.npy')
+        mfcc_delta2_path = os.path.join(mfcc_delta2_dir, 'mfccDelta2_' + audio_file[:16] + '.npy')
+        spectral_centroid_path = os.path.join(spectral_centroid_dir, 'cent_' + audio_file[:16] + '.npy')
+        spectral_bandwidth_path = os.path.join(spectral_bandwidth_dir, 'specBw_' + audio_file[:16] + '.npy') 
+        spectral_contrast_path = os.path.join(spectral_contrast_dir, 'contrast_' + audio_file[:16] + '.npy')
+        spectral_flatness_path = os.path.join(spectral_flatness_dir, 'flatness_' + audio_file[:16] + '.npy')
+        spectral_rolloff_path = os.path.join(spectral_rolloff_dir, 'rolloff_' + audio_file[:16] + '.npy')
+
+        if os.path.exists(mfcc_path) and os.path.exists(mfcc_delta_path) and os.path.exists(mfcc_delta2_path) and \
+            os.path.exists(spectral_centroid_path) and os.path.exists(spectral_bandwidth_path) and os.path.exists(spectral_contrast_path) and \
+            os.path.exists(spectral_flatness_path) and os.path.exists(spectral_rolloff_path):
+            continue
+        else:
+            y, sr = librosa.load(audio_path)
+            if not os.path.exists(mfcc_path):
+                mfcc = librosa.feature.chroma_cens(y=y, sr=sr)
+                np.save(mfcc_path, mfcc)
+            if not os.path.exists(mfcc_delta_path):
+                mfcc_delta = librosa.feature.delta(mfcc)
+                np.save(mfcc_delta_path, mfcc_delta)
+            if not os.path.exists(mfcc_delta2_path):
+                mfcc_delta2 = librosa.feature.delta(mfcc, order=2)
+                np.save(mfcc_delta2_path, mfcc_delta2)
+            if not os.path.exists(spectral_centroid_path):
+                cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+                np.save(spectral_centroid_path, cent)
+            if not os.path.exists(spectral_bandwidth_path):
+                spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+                np.save(spectral_bandwidth_path, spec_bw)
+            if not os.path.exists(spectral_contrast_path):
+                S = np.abs(librosa.stft(y))
+                contrast = librosa.feature.spectral_contrast(S=S, sr=sr)
+                np.save(spectral_contrast_path, contrast)
+            if not os.path.exists(spectral_flatness_path):
+                flatness = librosa.feature.spectral_flatness(y=y)
+                np.save(spectral_flatness_path, flatness)
+            if not os.path.exists(spectral_rolloff_path):       
+                rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+                np.save(spectral_rolloff_path, rolloff)
         
 
-    print('mfcc extracted from {} and stored at {}'.format(AUDIO_DIR, mfcc_dir))
-    print('mfcc_delta extracted from {} and stored at {}'.format(AUDIO_DIR, mfcc_delta_dir))
-    print('mfcc_delta2 extracted from {} and stored at {}'.format(AUDIO_DIR, mfcc_delta2_dir))
-    print('spectral_centroid extracted from {} and stored at {}'.format(AUDIO_DIR, spectral_centroid_dir))
-    print('spectral_bandwidth extracted from {} and stored at {}'.format(AUDIO_DIR, spectral_bandwidth_dir))
-    print('spectral_contrast extracted from {} and stored at {}'.format(AUDIO_DIR, spectral_contrast_dir))
-    print('spectral_flatness extracted from {} and stored at {}'.format(AUDIO_DIR, spectral_flatness_dir))
-    print('spectral_rollof extracted from {} and stored at {}'.format(AUDIO_DIR, spectral_rolloff_dir))
+    print('mfcc features of {} saved at {}'.format(AUDIO_DIR, mfcc_dir))
+    print('mfcc_delta features of {} saved at {}'.format(AUDIO_DIR, mfcc_delta_dir))
+    print('mfcc_delta2 features of {} saved at {}'.format(AUDIO_DIR, mfcc_delta2_dir))
+    print('spectral_centroid features of {} saved at {}'.format(AUDIO_DIR, spectral_centroid_dir))
+    print('spectral_bandwidth features of {} saved at {}'.format(AUDIO_DIR, spectral_bandwidth_dir))
+    print('spectral_contrast features of {} saved at {}'.format(AUDIO_DIR, spectral_contrast_dir))
+    print('spectral_flatness features of {} saved at {}'.format(AUDIO_DIR, spectral_flatness_dir))
+    print('spectral_rolloff features of {} saved at {}'.format(AUDIO_DIR, spectral_rolloff_dir))
 
-def extract_emotion_features():
+def extract_emotion_features(args):
 
     ''' extract predicted valence, arousal from PMEmo '''
     
@@ -130,30 +151,90 @@ def extract_emotion_features():
         extract_frame_feature(wavdir,lld_distdir,opensmiledir)
         process_dynamic_feature(lld_distdir,dynamic_distdir,all_dynamic_distfile,delimiter)
 
-    # extract_opensmile_features()
+    if args.extract_features:
+        extract_opensmile_features()
 
 
-    # predict valence and arousal using PMEmo
+    ''' predict static arousal and valence using PMEmo '''
     with open('features/emotions/tmp/static_features.arff') as f:
-        static_features = arff.load(f)
-    static_features = np.array(static_features['data'])[:, 1:-1]
-    
-    # ref: https://stackoverflow.com/questions/32700797/saving-a-cross-validation-trained-model-in-scikit
-    with open(os.path.join('features/emotions/tmp', 'svr_linear_arousal.pkl') , 'rb') as fid:
-        arousal_model = pickle.load(fid)
-    
-    with open(os.path.join('features/emotions/tmp', 'svr_linear_valence.pkl') , 'rb') as fid:
-        valence_model = pickle.load(fid)
+        arrf_data = arff.load(f)
+    static_features = np.array(arrf_data['data'])[:, 1:-1]
+    scaler = StandardScaler().fit(static_features)
+    scaled_dynamic_features = scaler.transform(static_features)
 
-    predicted_static_arousal = arousal_model.predict(static_features)
-    predicted_static_valence = valence_model.predict(static_features)
-    print(predicted_static_arousal.shape, predicted_static_valence.shape)
-    # TODO: save static arousal & valence, then extract dynamic labels
+    audio_filenames = [data[0][:-4] for data in arrf_data['data']]
+
+    # load saved model ref: https://stackoverflow.com/questions/32700797/saving-a-cross-validation-trained-model-in-scikit
+    with open('features/emotions/tmp/svr_linear_static_arousal.pkl' , 'rb') as fid:
+        static_arousal_model = pickle.load(fid)  
+    with open('features/emotions/tmp/svr_linear_static_valence.pkl' , 'rb') as fid:
+        static_valence_model = pickle.load(fid)
+
+    predicted_static_arousal = static_arousal_model.predict(scaled_dynamic_features)
+    predicted_static_valence = static_valence_model.predict(scaled_dynamic_features)
+
+    ''' predict dynamic arousal and valence using PMEmo '''
+    csv_data = pd.read_csv('features/emotions/tmp/dynamic_features.csv')
+    dynamic_features = csv_data[csv_data.columns[2:262]]
+    scaler = StandardScaler().fit(dynamic_features)
+    scaled_dynamic_features = scaler.transform(dynamic_features)
+    
+    with open('features/emotions/tmp/svr_rbf_dynamic_arousal.pkl' , 'rb') as fid:
+        dynamic_arousal_model = pickle.load(fid)
+    with open('features/emotions/tmp/svr_rbf_dynamic_valence.pkl' , 'rb') as fid:
+        dynamic_valence_model = pickle.load(fid)
+
+    predicted_dynamic_arousal = dynamic_arousal_model.predict(scaled_dynamic_features)
+    predicted_dynamic_valence = dynamic_valence_model.predict(scaled_dynamic_features)
+
+    ''' write static/dynamic arousal/valence to file '''
+    static_arousal_dir = 'features/emotions/static_arousal'
+    if not os.path.exists(static_arousal_dir):
+        os.makedirs(static_arousal_dir)
+    static_valence_dir = 'features/emotions/static_valence'
+    if not os.path.exists(static_valence_dir):
+        os.makedirs(static_valence_dir)
+    dynamic_arousal_dir = 'features/emotions/dynamic_arousal'
+    if not os.path.exists(dynamic_arousal_dir):
+        os.makedirs(dynamic_arousal_dir)
+    dynamic_valence_dir = 'features/emotions/dynamic_valence'
+    if not os.path.exists(dynamic_valence_dir):
+        os.makedirs(dynamic_valence_dir)
+
+    single_dynamic_prediction_len = predicted_dynamic_arousal.shape[0]//len(audio_filenames)
+    for idx in tqdm(range(len(audio_filenames)), desc='Emotion Features'):
+        static_arousal_path = os.path.join(static_arousal_dir, 'staticArousal_' + audio_filenames[idx] + '.npy')
+        static_valence_path = os.path.join(static_valence_dir, 'staticValence_' + audio_filenames[idx] + '.npy')
+        dynamic_arousal_path = os.path.join(dynamic_arousal_dir, 'dynamicArousal_' + audio_filenames[idx] + '.npy')
+        dynamic_valence_path = os.path.join(dynamic_valence_dir, 'dynamicValence_' + audio_filenames[idx] + '.npy')
+
+        if os.path.exists(static_arousal_path) and os.path.exists(static_valence_path) and os.path.exists(dynamic_arousal_path) and os.path.exists(dynamic_valence_path):
+            continue
+        else:
+            if not os.path.exists(static_arousal_path):
+                np.save(static_arousal_path, predicted_static_arousal[idx])
+            if not os.path.exists(static_valence_path):
+                np.save(static_valence_path, predicted_static_valence[idx])
+            if not os.path.exists(dynamic_arousal_path):
+                np.save(dynamic_arousal_path, predicted_dynamic_arousal[idx*single_dynamic_prediction_len : (idx+1)*single_dynamic_prediction_len])
+            if not os.path.exists(dynamic_valence_path):
+                np.save(dynamic_valence_path, predicted_dynamic_valence[idx*single_dynamic_prediction_len : (idx+1)*single_dynamic_prediction_len])
+
+    print('static arousal of {} saved at {}'.format(AUDIO_DIR, static_arousal_dir))
+    print('static valence of {} saved at {}'.format(AUDIO_DIR, static_valence_dir))
+    print('dynamic arousal of {} saved at {}'.format(AUDIO_DIR, dynamic_arousal_dir))
+    print('dynamic valence of {} saved at {}'.format(AUDIO_DIR, dynamic_valence_dir))
+
 
 
 if __name__ == '__main__':
-    # extract_chord_features()
-    # extract_timbre_features()
-    extract_emotion_features()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--extract_features', help='use opensmile to extract features or not', type=bool, default=False)
+    args = parser.parse_args()
+
+
+    extract_chord_features()
+    extract_timbre_features()
+    extract_emotion_features(args)
 
     
